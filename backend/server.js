b0888a88da2c137e3e3ac9fe4aff2e277bd95978
Rajiv <kumarrajiv0920@gmail.com>
2026-07-15 15:27:01 +0530

    Add health route to active the API all the time

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,57 @@
-import express from 'express'
-import 'dotenv/config'
-import path from 'path'
-import cors from 'cors'
+import express from 'express';
+import 'dotenv/config';
+import path from 'path';
+import cors from 'cors';
 
-import { connectDB } from './config/db.js'
-import taskRoutes from './routes/task.route.js'
+import { connectDB } from './config/db.js';
+import taskRoutes from './routes/task.route.js';
 
-const app = express()
-const PORT = process.env.PORT
-const __dirname = path.resolve()
+const app = express();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://kanban-lite.vercel.app',
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
+);
 
-app.use('/api/tasks', taskRoutes)
+app.use('/api/tasks', taskRoutes);
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
-  })
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
 }
 
 const startServer = async () => {
   try {
-    await connectDB()
+    await connectDB();
     app.listen(PORT, () => {
-      console.log(`Server is running on the port: ${PORT}`)
-    })
+      console.log(`Server is running on the port: ${PORT}`);
+    });
   } catch (error) {
-    console.log(`Server failed to start: ${error}`)
+    console.log(`Server failed to start: ${error}`);
   }
-}
+};
 
-startServer()
+startServer();
